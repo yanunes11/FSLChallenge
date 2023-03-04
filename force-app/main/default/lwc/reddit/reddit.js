dcb21448ebd5c9f2d749e1a5e5e9b8888f8ae97a
yanunes11 <yannunes112@gmail.com>
2023-03-04 17:22:34 -0300

    feat: show only 40 datable

--- force-app/main/default/lwc/reddit/reddit.js
+++ force-app/main/default/lwc/reddit/reddit.js
@@ -5,12 +5,13 @@ const columns = [
     { label: 'Title', fieldName: 'Title__c', type: 'text'},
     { label: 'Full Name', fieldName: 'Full_Name__c', type: 'text'},
     { label: 'Thumbnail ', fieldName: 'SelfText__c', type: 'url' },
-    { label: 'SelfText', fieldName: 'Thumbnail__c', type: 'text'},
+    { label: 'SelfText', fieldName: 'Thumbnail__c', type: 'url'},
 ];
 
 export default class Reddit extends LightningElement {
     data = [];
     columns = columns;
+    rowOffset = 0;
 
 
     connectedCallback() {
